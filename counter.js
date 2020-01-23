33b9c33c35eff16e99cb68e6954252c138fc93d8
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-01-23 23:52:44 +0200

    feat:display number of words

--- counter.js
+++ counter.js
@@ -1,10 +1,13 @@
 const t = document.querySelector('textarea');
 const charDOM = document.querySelector('#charNum')
+const wordDOM = document.querySelector('#wordNum')
 let chars, words = 0
 
 t.addEventListener('input', event => {
-const text = t.value
+const text = t.value.trim()
     
     charDOM.innerText = text.length
+    wordDOM.innerText = text.split(" ").length
+
     
 })
